b352010bce93245539d2a50366c77d2516831bf7
janupasilva2005 <janupasilva2005@gmail.com>
2023-04-27 19:03:44 +0530

    Implemented player movement

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ const info = {
 let currentX = 0;
 let currentY = 0;
 
+let playerX = 0;
+
 class Brick {
   constructor(x, y, width, height) {
     this.x = x;
@@ -36,6 +38,48 @@ class Brick {
     }
   }
 }
+
+class Player {
+  constructor(x) {
+    this.width = 200;
+    this.height = 20;
+    this.x = x;
+    this.y = cv.height - info.dy;
+    this.dx = 10;
+  }
+
+  draw() {
+    ctx.lineWidth = 2;
+
+    ctx.beginPath();
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.fill();
+    ctx.stroke();
+  }
+
+  update(direction) {
+    if (direction === "right") {
+      this.x += this.dx;
+      playerX = this.x;
+    }
+
+    if (direction === "left") {
+      this.x -= this.dx;
+      playerX = this.x;
+    }
+  }
+}
+
+const player = new Player(playerX);
+
+/**
+ * Rendering all bricks
+ */
+const renderBricks = () => {
+  for (let k = 0; k < bricksArray.length; k++) {
+    bricksArray[k].draw();
+  }
+};
 
 /**
  * Initialize everything
@@ -60,12 +104,33 @@ const init = () => {
     currentY += info.dy;
   }
 
-  /**
-   * Rendering all bricks
-   */
-  for (let k = 0; k < bricksArray.length; k++) {
-    bricksArray[k].draw();
-  }
+  renderBricks();
+
+  player.draw();
 };
 
+// Calling init function
 init();
+
+const animate = () => {
+  ctx.clearRect(0, 0, cv.width, cv.height);
+
+  // draw bricks
+  renderBricks();
+
+  player.draw();
+
+  requestAnimationFrame(animate);
+};
+
+animate();
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowRight") {
+    player.update("right");
+  }
+
+  if (e.key === "ArrowLeft") {
+    player.update("left");
+  }
+});
